4ec32404e80cb106c9f27d736a4d580f4977b618
swiperwag <jameslenovoa536@gmail.com>
2024-02-15 16:24:32 +0800

    update project intro

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let intro = document.querySelector('.intro');
 
 addEventListener('DOMContentLoaded', function() {
+  intro.style.display = 'grid';
+
+
   setTimeout(() => {
     intro.style.display = 'none';
   }, 1400);
